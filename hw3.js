c30d34f4d25157734dac58f761db17c1d0e226e1
Zhanna Romaniuk <zhasteff@ukr.net>
2025-04-13 19:35:24 +0300

    js_homework3

---
+++ hw3.js
@@ -0,0 +1,20 @@
+let x = 2;
+let y =10;
+
+for (i = 0; i <=y; i++) {
+if (i === 0) {
+    console.log(1);}
+if (i === 1) {
+    console.log(x);}
+if (i === 2) {
+    var a = x*x;
+    console.log(a);}
+
+else { 
+        pow (x,y);}
+
+    function pow (x,y) {
+    let b = a*x;
+    console.log(b);
+    a = b;}
+}
