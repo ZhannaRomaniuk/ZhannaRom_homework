7bf54b67e6afe2cb7459cc9bcfe83e160a0acf58
Zhanna Romaniuk <zhasteff@ukr.net>
2025-05-01 17:38:23 +0300

    Update hw7_1.js

--- hw7_1.js
+++ hw7_1.js
@@ -16,5 +16,20 @@ var arr = [
     },
 ];
 
-let result = arr.filter( (el) => (el.email || el["<email"]) | el.email.search(/(([\w]+.[\w]+)|[\w]+)@((yahoo.com)|(gmail.com))/gm));
-console.log(result);
+let re = /^[a-zA-Z0-9]+[.]?[a-zA-Z0-9]+(@yahoo.com|@gmail.com)$/gm;
+function getArrayWithCorrectEmails (arr,regex) {
+    let arrayWithEmails = [];
+    arr.forEach(function (currentValue){
+        for (const key in currentValue){
+            if (key ==="email" || key ==="<email")
+                arrayWithEmails.push(currentValue[key]);
+        }
+    })
+    console.log(arrayWithEmails);
+return arrayWithEmails.filter(function (email) {
+return regex.test(email);
+});
+}
+
+let newArr = getArrayWithCorrectEmails(arr,re);
+console.log(newArr);
